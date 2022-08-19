82e2b8c504e88de75c908dca75004ea11f9b2bc1
Artem <98690512+ArtemTeplov@users.noreply.github.com>
2022-08-19 20:21:55 +0300

    Add files via upload

---
+++ src/components/main-information/main-information.jsx
@@ -0,0 +1,13 @@
+import Sort from "../sort/sort";
+import Tickets from "../tickets/tickets";
+
+const MainInformation = () => {
+    return (
+        <div className="main-information">
+            <Sort />
+            <Tickets />
+        </div>
+    )
+}
+
+export default MainInformation;
